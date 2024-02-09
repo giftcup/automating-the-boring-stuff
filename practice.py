36a1b2c65cb031bbb567cb10a6b7737f26135d09
giftcup <salometambe@gmail.com>
2024-02-09 21:37:28 +0100

    input-output

---
+++ practice.py
@@ -0,0 +1,8 @@
+username = input("What's your name: ")
+print("Hello", username)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+print(f"{num1} + {num2} = {int(num1) + int(num2)}")
+print(type(num1))
